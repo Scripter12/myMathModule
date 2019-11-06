307d85757657c19419780f937bfdbff67d84749c
kadjiah <kadijahwilson@pursuit.org>
2019-11-05 20:53:14 -0500

    combine mul to index

---
+++ index.js
@@ -0,0 +1,14 @@
+const mul = require('./sum.js');
+
+/* 
+  myMathModule
+
+  This is a collaboratively made Math module with support for basic math operations.
+  Feel free to use it for you basic math needs.
+*/
+const Math = {
+    PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    mul: mul
+  }
+  
+  module.exports = Math
